6faa5e26ebf99894a896c03751c8dc23d3862920
Théotim ALBERTEAU <sunshouwu@outlook.com>
2026-01-12 10:01:33 +0100

    Add Base Files

---
+++ src/main/java/com/example/APIGestionArticle/ApiGestionArticleApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIGestionArticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionArticleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/APIGestionArticle/ApiGestionArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIGestionArticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
